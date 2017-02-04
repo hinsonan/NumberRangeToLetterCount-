9530ecc5f7758ab6ee4d2c75b2d4cd4c305717c6
hinsonan <hinsonan@gmail.com>
2017-02-04 16:03:32 -0600

    Adds comments

--- NumberRangeToLetterCount/NumberRangeToLetterCount/Program.cs
+++ NumberRangeToLetterCount/NumberRangeToLetterCount/Program.cs
@@ -29,32 +29,9 @@ namespace NumberRangeToLetterCount
                 //validates that the user inputs the correct number
             } while ((min < -100000) || (max > 100000) || min > max);
 
-
-            //create an array that will hold all the ints from min to max
-            int[] intArray = createIntArray(min, max);
-
-            //for each number in the array display the word for that number and put that in numberWords array
-            string[] numberWords = new string[(max - min) + 1];
-            
-            for(int i = 0; i < intArray.Length; i++)
-            {
-                numberWords[i] = NumberToWords(intArray[i]);
-            }
-
-
-            
-            ulong wordToNumberCount = 0;
-            for (int m = 0; m < numberWords.Length; m++)
-            {
-                
-                wordToNumberCount += (ulong)numberWords[m].Length;
-            }
-
-            Console.WriteLine("Total number of words added: " + wordToNumberCount);
-
-
-
-
+            //calls the function that will do all the work to output the result
+            NRtLC(min, max);
+            Console.ReadKey();
 
         }
 
@@ -92,46 +69,80 @@ namespace NumberRangeToLetterCount
             string words = "";
             //these next block of if statments will divide a number then call the function against the divided number
             //then it gives the remainder and keeps going down the if statements and calling the function over until the number is converted to words
+            // will diveide the number by 1,000,000 if the number is above a million
+            //now since the user can't enter a number higher than 100,000 this first statement wont be called but if we need to tweak the number range we can
             if ((number / 1000000) > 0)
             {
                 words += NumberToWords(number / 1000000) + "million";
                 number %= 1000000;
             }
-
+            //will dive the number by 1000 if possible
             if ((number / 1000) > 0)
             {
                 words += NumberToWords(number / 1000) + "thousand";
                 number %= 1000;
             }
-
+            //will divide the number by 100 if possible
             if ((number / 100) > 0)
             {
                 words += NumberToWords(number / 100) + "hundred";
                 number %= 100;
             }
-
+            //if the number is above zero at this point then the words are special cases so we will make an array for them
             if (number > 0)
             {
                 
-
-                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six",
+                //creates string array for the special cases for 0-19
+                var units = new[] { "zero", "one", "two", "three", "four", "five", "six",
                     "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen",
                     "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
 
-                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty",
+                //creates string array for the special cases of tens
+                var tens = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty",
                     "seventy", "eighty", "ninety" };
 
+                //when the number is below 20 it will fall into one of the special cases array
                 if (number < 20)
-                    words += unitsMap[number];
+                    words += units[number];
                 else
                 {
-                    words += tensMap[number / 10];
+                    words += tens[number / 10];
                     if ((number % 10) > 0)
-                        words += unitsMap[number % 10];
+                        words += units[number % 10];
                 }
             }
 
             return words;
+        }
+
+        private static void NRtLC(int min, int max)
+        {
+
+            //create an array that will hold all the ints from min to max
+            int[] intArray = createIntArray(min, max);
+
+            //for each number in the array display the word for that number and put that in numberWords array
+            string[] numberWords = new string[(max - min) + 1];
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                numberWords[i] = NumberToWords(intArray[i]);
+            }
+
+
+
+            ulong wordToNumberCount = 0;
+            for (int m = 0; m < numberWords.Length; m++)
+            {
+
+                wordToNumberCount += (ulong)numberWords[m].Length;
+            }
+
+            Console.WriteLine("Number to Letter Count: " + wordToNumberCount);
+
+
+
+
         }
 
     }
